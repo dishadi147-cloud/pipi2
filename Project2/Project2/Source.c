dd55d963adaa1a6bc5c4f87cfc67a50ab0b137d6
vankxwnk <ivanivanov140706@gmail.com>
2026-01-16 08:17:27 +0300

    1111

--- Project2/Project2/Source.c
+++ Project2/Project2/Source.c
@@ -3,3 +3,18 @@
 int main() {
   
         printf("„исло %d - чЄтное.\n", number);
+        int main() {
+            int number;
+
+            printf("¬ведите целое число: ");
+            scanf("%d", &number); // —читываем число с клавиатуры
+
+            if (number % 2 == 0) {
+                printf("„исло %d - чЄтное.\n", number);
+            }
+            else {
+                printf("„исло %d - нечЄтное.\n", number);
+            }
+
+            return 0;
+        }
